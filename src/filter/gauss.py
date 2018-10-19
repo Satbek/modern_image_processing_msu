647d59e289360e55d0e1590b8a1f130a50f92e4c
SatbekTurganbayev <set.turg@mail.ru>
2018-10-19 11:14:02 +0300

    fix gauss func

--- src/filter/gauss.py
+++ src/filter/gauss.py
@@ -1,12 +1,13 @@
 import math
 from util.math.convolution import convolve1d
 
-def _gauss_func1d(x, sigma):
+def gauss_func1d(x, sigma):
     return math.exp(-0.5 * (x**2) / sigma**2) / (math.sqrt(2 * math.pi * sigma ** 2))
 
-def _gauss_filter1d(sigma):
-    sigma = int(sigma + 1)
-    return [_gauss_func1d(i, sigma) for i in range(-3 * sigma, 3 * sigma + 1)]
+def gauss_filter1d(sigma):
+    sd = int(sigma + 1)
+    result = [gauss_func1d(i, sigma) for i in range(-3 * sd, 3 * sd + 1)]
+    return result
 
 def gauss(array, mode, sigma):
     """
@@ -16,6 +17,7 @@ def gauss(array, mode, sigma):
     sigma: радиус размытия
     result: двумерный массив
     """
-    kernel = _gauss_filter1d(sigma)
+    kernel = gauss_filter1d(sigma)
     tmp = convolve1d(array, kernel, mode, axis = 1)
-    return convolve1d(tmp, kernel, mode, axis = 0)
+    result = convolve1d(tmp, kernel, mode, axis = 0)
+    return result
